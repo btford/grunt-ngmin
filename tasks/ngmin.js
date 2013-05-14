80559eaad19b12a1b4191d8ae1a870b75cc6e07e
Olivier Louvignes <olivier@mg-crea.com>
2013-05-14 20:06:33 +0200

    feat(banner): added support for banner & footer

--- tasks/ngmin.js
+++ tasks/ngmin.js
@@ -6,15 +6,27 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('ngmin', 'Annotate AngularJS scripts for minification', function () {
 
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      banner: '',
+      footer: ''
+    });
+
+    // Process banner and footer.
+    var banner = grunt.template.process(options.banner);
+    var footer = grunt.template.process(options.footer);
+
     grunt.log.writeln('ngminifying ' + grunt.log.wordlist(this.files.map(function (file) {
       return file.src;
     })));
 
     this.files.forEach(function (file) {
-      var contents = file.src
+      var contents = banner + file.src
         .map(grunt.file.read)
         .map(ngmin.annotate)
-        .join('');
+        .join('') + footer;
+
+      // Write the destination file.
       grunt.file.write(file.dest, contents);
     });
 
